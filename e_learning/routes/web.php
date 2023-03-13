b1ec318575dca504d05fc7ebec12a85e53c4d3b2
Rai <mraihanzhafran.14@gmail.com>
2023-03-13 20:45:06 +0700

    adding some rute in the link

--- e_learning/routes/web.php
+++ e_learning/routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/lr/teacher', function () {
+    return view('lr/teacher');
+});
+
+Route::get('/lr/student', function () {
+    return view('lr/student');
 });
